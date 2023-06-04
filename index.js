d8b0a0f686b5003c90971554542d39e7ca40acfa
framnz85 <clavmall.85@gmail.com>
2023-06-04 10:31:34 +0800

    uploading images

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.listen(8001, () => console.log("API running on port 5001"));
+app.listen(3001, () => console.log("API running on port 5001"));
 
 app.get("/", (req, res) => {
   res.json("My API running...");
